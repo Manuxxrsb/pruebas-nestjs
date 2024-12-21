f11d7a663547dc906ad461e6f0d944ff45fb0877
Manuxxrsb <manuxxrsb@gmail.com>
2024-12-21 10:40:01 -0300

    Agregar controlador Hello y métodos para tareas; actualizar módulo de tareas y usuarios con ayuda de @ExpressJs

--- src/Tasks/Tasks.controller.ts
+++ src/Tasks/Tasks.controller.ts
@@ -1,23 +1,35 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Patch, Post, Put } from '@nestjs/common';
 import { TaskService } from './Task.service';
 //los controllers sirven para agregar la logica de programacion de la aplicacion
-@Controller({})
+@Controller('/tasks') //ruta del endpoint para acceder a los metodos del controller
 export class TasksController {
   ServicioTarea: TaskService;
   constructor(ServicioTarea: TaskService) {
     this.ServicioTarea = ServicioTarea;
   }
 
-  @Get('/tasks')
-  getAllTasks() {
+  //metodo que retorna un string , en caso de no ser string lo que retorne falla por TypeError
+  @Get()
+  getAllTasks(): string {
     return 'Obtengo todas las tareas';
   }
-  @Get('/task')
-  get_task() {
-    return 'Obtengo una tarea';
+
+  @Post()
+  createTasks(): string {
+    return 'Creo una tarea';
   }
 
-  @Get('/taskSaludo')
+  @Put()
+  UpdateTasks(): string {
+    return 'Update de una tarea';
+  }
+
+  @Patch()
+  UpdatepatchTasks() {
+    return 'actualice un valor del objeto tarea';
+  }
+
+  @Get('/Saludo') //ruta personalizada para acceder a un metodo especifico
   get_taskSaludo() {
     return this.ServicioTarea.getSaludo();
   }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
 
+interface User {
+  id: number;
+  name: string;
+}
+
 @Controller('users')
 export class UsersController {
-  @Get('/users')
+  @Get()
   getAllUsers() {
     return 'Obtengo todos los usuarios';
   }
+
+  @Get('/usuario') //al retornar un objeto user no falla
+  getUser(): User {
+    return {
+      id: 1,
+      name: 'Manuel Solis',
+    };
+  }
+  v;
 }
